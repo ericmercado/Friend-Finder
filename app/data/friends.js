74e58764e6f684b445acd06bda7a4e55f930155b
Eric <eric.mercado60@hotmail.com>
2018-02-27 18:45:32 -0600

    final

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Harvey Weinstein",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Harvey_Weinstein_2011_Shankbone.JPG/1200px-Harvey_Weinstein_2011_Shankbone.JPG",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "John 'the Kid Diddler' Podesta",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/John_Podesta_official_WH_portrait.jpg/220px-John_Podesta_official_WH_portrait.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Marina 'Spirit Cooking Whore' Abramovic",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Marina_Abramovi%C4%87_-_The_Artist_Is_Present_-_Viennale_2012_%28cropped%29.jpg/1200px-Marina_Abramovi%C4%87_-_The_Artist_Is_Present_-_Viennale_2012_%28cropped%29.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Hillary 'Rape enabler' Clinton",
+    "photo" : "http://www.strangepolitics.com/images/content/200622.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Gavin 'Stink Smeller' McInnes",
+    "photo" : "https://rewire.news/wp-content/uploads/2017/08/mcinnes-740x525.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Chelsea Hubbell",
+    "photo" : "http://images.gawker.com/mpbsrp7z2rwim47iqziq/c_scale,fl_progressive,q_80,w_800.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Alex Jones",
+    "photo" : "https://yt3.ggpht.com/a-/AJLlDp2l2wgSQ6-3FvzVl3M4qsJA1QlUnWUpU7xKKw=s900-mo-c-c0xffffffff-rj-k-no",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "R. Budd Dwyer",
+    "photo" : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/R._Budd_Dwyer.jpg/220px-R._Budd_Dwyer.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Andre",
+    "photo" : "http://images.mentalfloss.com/sites/default/files/styles/width-constrained-728/public/501418-BusinessWire3_0.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Krissy",
+    "photo" : "https://i.ytimg.com/vi/Vdtn7xrG3FI/hqdefault.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Zob",
+    "photo" : "http://susers.thatsmyface.com/b/bloobaaed656/Dylan_Pitt_fg_0pAJZoBToX.fg.front-thumbnail_2f8ca99a.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Murderer of children",
+    "photo" : "https://peopledotcom.files.wordpress.com/2017/03/casey-cover-450x450.jpg?w=450",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "George Soros",
+    "photo" : "https://specials-images.forbesimg.com/imageserve/59d50e1f4bbe6f37dd9ff7dd/416x416.jpg?background=000000&cropX1=931&cropX2=2948&cropY1=485&cropY2=2501",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "Muhammad",
+    "photo" : "https://qph.fs.quoracdn.net/main-qimg-82f956559f63cf823fd7cdd8fe2dc703",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
